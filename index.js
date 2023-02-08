c97a0bec0378c8512b88a74283bc7c4603d411ae
MANEERAT MING <ming.maneerat@allianz.com>
2023-02-08 22:17:45 +0700

    Initial release

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-let { IncomingWebhook } = require('@slack/client');
-let markdowntable = require('markdown-table');
-let prettyms = require('pretty-ms');
 
-class SlackReporter {
+class LineReporter {
     constructor(emitter, reporterOptions) {
-        const backticks = '```';
-        const webhookUrl = process.env.SLACK_WEBHOOK_URL || reporterOptions.webhookUrl;
-        const channel = process.env.SLACK_CHANNEL || reporterOptions.channel;
-        let title = process.env.TITLE || reporterOptions.title;
-        let header = process.env.HEADER || reporterOptions.header || '';
+        const token = process.env.LINE_TOKEN || reporterOptions.token;
 
-        if (!webhookUrl) {
-            console.log('please provide slack webhook url');
+        if (!token) {
+            console.log('please provide Line token');
             return;
         }
-
-        let verbose = process.argv.includes('--verbose');
 
         emitter.on('done', (err, summary) => {
             if (err) {
@@ -23,63 +14,23 @@ class SlackReporter {
             }
             let run = summary.run;
             let totalFailures = summary.run.failures;
-            let data = [];
-            if (!title) {
-                title = summary.collection.name;
-                if (summary.environment.name) {
-                    title += ' - ' + summary.environment.name;
-                }
-            }
-            let headers = [header, 'total', 'failed'];
-            let headersFailures = ['request failures', 'assertion', 'error'];
-            let arr = ['iterations', 'requests', 'testScripts', 'prerequestScripts', 'assertions'];
 
-            data.push(headers);
-            arr.forEach((element) => {
-                data.push([element, run.stats[element].total, run.stats[element].failed]);
+            let text = 'Alert:';
+
+            for (let failure of totalFailures) {
+                text += '\n\nCase: ' + failure.error.test;
+            }
+
+            const lineNotify = require('line-notify-nodejs')(token);
+
+            lineNotify.notify({
+              message: text,
+            }).then(() => {
+              console.log('send completed!');
             });
 
-            let duration = prettyms(run.timings.completed - run.timings.started);
-            data.push(['------------------', '---------------------------------', '------------------------------']);
-            data.push(['total run duration', duration]);
-            data.push(['------------------', '---------------------------------', '------------------------------']);
-            if (verbose) {
-                data.push(headersFailures);
-                data.push([
-                    '------------------',
-                    '---------------------------------',
-                    '------------------------------',
-                ]);
-
-                function pushErrors(index) {
-                    let failures = summary.run.failures[index];
-                    let request = failures.source.name;
-                    let failuresTest = failures.error.test;
-                    let failuresMessage = failures.error.message;
-                    data.push([request, failuresTest, failuresMessage]);
-                }
-                totalFailures.forEach((failure, index) => pushErrors(index));
-            }
-
-            let table = markdowntable(data);
-            let text = `${title}\n${backticks}${table}${backticks}`;
-            let msg = {
-                text: text,
-            };
-
-            if (channel) {
-                msg['channel'] = channel;
-            }
-
-            const webhook = new IncomingWebhook(webhookUrl);
-            webhook.send(msg, (error, response) => {
-                if (error) {
-                    return console.error(error.message);
-                }
-                console.log(response);
-            });
         });
     }
 }
 
-module.exports = SlackReporter;
+module.exports = LineReporter;
